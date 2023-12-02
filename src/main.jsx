8f05e89c20a8de7df97253291e18198a116e3c7a
Dee <deepakdilse@gmail.com>
2023-12-01 16:44:49 -0800

    add client side routing and palce holder about me and porjects page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './index.css'
 
+import App from './App.jsx'
+import ProjectPage from './pages/ProjectsPage.jsx';
+import AboutMePage from './pages/AboutMePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        index: true,
+        element: <AboutMePage/>,
+      },
+      {
+        path: 'projects',
+        element: <ProjectPage/>,
+      }
+    ],
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+   <RouterProvider router={router} />
+
 )
